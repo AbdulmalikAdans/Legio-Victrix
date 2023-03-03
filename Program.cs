3d6b562a172cf22dac1b6709923efef61c795414
Surge <poptropist14@gmail.com>
2023-03-03 21:42:33 +1000

    Simple Ai there

--- Program.cs
+++ Program.cs
@@ -4,6 +4,50 @@ namespace Legio_Victrix
 {
     public class Program
     {
-        
+        static void Main(string[] args)
+        {
+            bool shutdown = false;
+            bool foundResponse;
+            string inputValue;
+            string outputValue = "";
+            string aiName = "Legio Victrix";
+
+            Console.WriteLine(aiName + ": Hello");
+
+            while (!shutdown)
+            {
+                foundResponse = false;
+                Console.WriteLine("User:");
+                inputValue = Console.ReadLine().ToLower();
+                
+                Console.WriteLine(aiName + ": ");
+
+                //response
+
+                switch (inputValue)
+                {
+                    case "hello":
+                    outputValue = "Hello to you! How are you?";
+                    foundResponse = true;
+                    break;
+                    case "exit":
+                    Console.WriteLine("Good bye");
+                    Thread.Sleep(3000);
+                    //Console.ReadKey();
+                    Environment.Exit(0);
+                    break;
+                }
+
+                // if response is found
+                if (foundResponse)
+                {
+                    Console.WriteLine(outputValue);
+                }
+                else
+                {
+                    Console.WriteLine("I do not understand. Can you please rephrase it?");
+                }
+            }
+        }
     }
 }
