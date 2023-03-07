4e8d8179f6fd38499d04a1d5d66f0a5134d91bbd
Atlas <poptropist14@gmail.com>
2023-03-07 23:16:30 +0000

    just fixed some bugs

--- Program.cs
+++ Program.cs
@@ -6,21 +6,24 @@ namespace Legio_Victrix
     {
         static void Main(string[] args)
         {
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
             bool shutdown = false;
             bool foundResponse;
             string inputValue;
             string outputValue = "";
             string aiName = "Legio Victrix";
-
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(aiName + ": Hello");
+            Console.ResetColor();
 
             while (!shutdown)
             {
                 foundResponse = false;
-                Console.WriteLine("User:");
+                Console.WriteLine(userName);
                 inputValue = Console.ReadLine().ToLower();
-                
+                Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine(aiName + ": ");
+                Console.ResetColor();
 
                 //response
 
@@ -30,22 +33,33 @@ namespace Legio_Victrix
                     outputValue = "Hello to you! How are you?";
                     foundResponse = true;
                     break;
-                    case "exit":
+
+                    case "exit":                
                     Console.WriteLine("Good bye");
                     Thread.Sleep(3000);
                     //Console.ReadKey();
                     Environment.Exit(0);
                     break;
+                    
+                    case "what is the meaing of life":
+                    outputValue = "I really dont know!";
+                    foundResponse = true;
+                    break;
+
                 }
 
                 // if response is found
                 if (foundResponse)
                 {
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine(outputValue);
+                    Console.ResetColor();
                 }
                 else
                 {
-                    Console.WriteLine("I do not understand. Can you please rephrase it?");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("boi you stupid please rephrase that?");
+                    Console.ResetColor();
                 }
             }
         }
